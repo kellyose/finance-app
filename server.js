9bf8856e956387554bd30f5830ad2491114edd56
kellyose <171274602+kellyose@users.noreply.github.com>
2025-12-15 09:38:24 +0100

    Update CORS to allow frontend URL

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5176', 'http://localhost:5174', 'http://localhost:5173'], // add your frontend host here later
+  origin: ['https://finance-apps.onrender.com'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
